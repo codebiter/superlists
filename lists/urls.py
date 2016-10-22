8e6e488de369afe4491e3ff4b3743da31ada79d7
codebiter <rahuliamthesarma@gmail.com>
2016-10-23 01:34:29 +0530

    enforce model validation in list view

--- lists/urls.py
+++ lists/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
 from lists import views
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
-    # url(r'^admin/', admin.site.urls),
 ]
